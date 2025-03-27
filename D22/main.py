a935842b134916aaddb6168083b6a2c60d4c4cbb
extremenoob88 <iwanttolearndocker88@outlook.com>
2025-03-27 10:59:11 +0100

    adding the paddle

--- D22/main.py
+++ D22/main.py
@@ -1,9 +1,16 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 
 screen.setup(width=800, height=600)
 screen.title("Pong")
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(0, 0)
 
 screen.exitonclick()
