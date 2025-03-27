2180444b0e547b91599288284255f7ad358ec36b
extremenoob88 <iwanttolearndocker88@outlook.com>
2025-03-27 11:43:45 +0100

    clean-up

--- D22/main.py
+++ D22/main.py
@@ -11,23 +11,11 @@ screen.title("Pong Game")
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-def r_paddle_go_up():
-    r_paddle.go_up()
-
-def r_paddle_go_down():
-    r_paddle.go_down()
-
-def l_paddle_go_up():
-    l_paddle.go_up()
-
-def l_paddle_go_down():
-    l_paddle.go_down()
-
 screen.listen()
-screen.onkey(r_paddle_go_up, "Up")
-screen.onkey(r_paddle_go_down, "Down")
-screen.onkey(l_paddle_go_up, "w")
-screen.onkey(l_paddle_go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 
